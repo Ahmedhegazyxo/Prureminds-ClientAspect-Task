06fe332d75182b3d1f0881bc6709a5518ee6c428
ahmed 30981519 <ahmed30981519@f-eng.tanta.edu.eg>
2025-01-08 07:43:21 +0200

    adding relevant questios.razor

--- Pureminds.Client/Pages/Breif.razor.cs
+++ Pureminds.Client/Pages/Breif.razor.cs
@@ -2,5 +2,19 @@
 
 public partial class Breif
 {
-
+    GeneralSetting? generalSetting = new GeneralSetting();
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            List<GeneralSetting>? generalSettings = await _client.GetFromJsonAsync<List<GeneralSetting>>("api/GeneralSettings");
+            generalSetting = generalSettings.FirstOrDefault();
+            if (generalSettings is null)
+                throw new Exception("General Settings are not set");
+        }
+        catch (Exception exception)
+        {
+        }
+        await base.OnInitializedAsync();
+    }
 }
---
+++ Pureminds.Client/Pages/RelevantQuestions.razor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Pureminds.Client.Pages;
+
+public partial class RelevantQuestions
+{
+    [Parameter]
+    public int Id { get; set; }
+    bool isNotFoundSource = false;
+    RelevantQuestion? relevantQuestion;
+    private string questionAnswer;
+    private int charIndex = 0;
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            relevantQuestion = await _client.GetFromJsonAsync<RelevantQuestion>($"api/RelevantQuestions/GetById/{Id}");
+            if(relevantQuestion is not null)
+            {
+                await StartTypeWriterEffect(relevantQuestion.Answer);
+            }
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception.Message);
+            isNotFoundSource = true;    
+        }
+        await base.OnInitializedAsync();
+    }
+    private async Task StartTypeWriterEffect(string text)
+    {
+        questionAnswer = string.Empty;
+        charIndex = 0;
+
+        while (charIndex < text.Length)
+        {
+            questionAnswer += text[charIndex];
+            charIndex++;
+            StateHasChanged(); 
+            await Task.Delay(25); 
+        }
+    }
+}
