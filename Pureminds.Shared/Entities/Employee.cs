1807d32816cd19ec2d8ad498e6c3afdeee511295
ahmed 30981519 <ahmed30981519@f-eng.tanta.edu.eg>
2025-01-10 00:21:12 +0200

    adding board members

---
+++ Pureminds.Shared/Entities/Employee.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Pureminds.Shared;
+
+public class Employee : BaseEntity
+{
+    public string Name { get; set; }
+    public string ImagePath { get; set; }
+    public string JobDescription { get; set; }  
+}
